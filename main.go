d666edf64b0cd35ebd9c6db019183cf8c50db602
kasepuu <joel.meeks@outlook.com>
2023-03-27 02:32:00 +0300

    upload

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Incorrect usage!")
+		printUsage()
+	}
+	app.wGet
+	app.wGet(os.Args[2]) // wget
+}
+
+func printUsage() {
+	fmt.Println("USAGE:")
+	fmt.Println("go run . <download_link>")
+	fmt.Println("EXAMPLE:")
+	fmt.Println("go run . https://pbs.twimg.com/media/EMtmPFLWkAA8CIS.jpg")
+	os.Exit(0)
+}
