5fc1f1546ab4be89bd78fd6d644d66372ccb8eee
juss <jussmartson21@gmail.com>
2023-03-28 17:14:16 +0300

    downloadfixed

--- wget/functions.go
+++ wget/functions.go
@@ -47,23 +47,25 @@ func sliceUrl(url string) (rurl, cleanurl, givenfilename, givenpath, httpmethod 
 }
 
 // Make the GET request to a url, return the response
-func getResponse(link, httpmethod, shorturl, fileName string) *http.Response {
+func getResponse(link, httpmethod, shorturl, fileName string) (*http.Response, string) {
 	net.LookupPort("tcp", httpmethod)
 
 	u, err := url.Parse(link)
+	errorChecker(err)
 	Port := GetPort(u.Scheme)
 
 	doLogging("Resolving "+shorturl+" ("+shorturl+")... "+httpmethod+" "+shorturl, true)
-	tr := new(http.Transport)
+	//tr := new(http.Transport)
 	doLogging("Connecting "+shorturl+" ("+shorturl+")|"+httpmethod+"|:"+Port+"...", false)
 
-	client := &http.Client{Transport: tr}
-	req, err := http.NewRequest("GET", link, nil)
+	//client := &http.Client{Transport: tr}
+	req, err2 := http.NewRequest("GET", link, nil)
+	errorChecker(err2)
 	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
 	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
-	resp, err := client.Do(req)
+	resp, err3 := http.DefaultClient.Do(req)
 	//resp, err := client.Get(link)
-	errorChecker(err)
+	errorChecker(err3)
 	doLogging(" connected.", true)
 	doLogging(httpmethod+"request sent, awaiting response... "+resp.Status, true)
 	if resp.StatusCode != 200 {
@@ -73,13 +75,13 @@ func getResponse(link, httpmethod, shorturl, fileName string) *http.Response {
 
 	doLogging("", true)
 
-	size, filetype := FileInfo(resp, fileName, link)
+	size, filetype := FileInfo(fileName, link)
 	a := strconv.FormatInt(size, 10)
 	doLogging("Length:"+a+CalcSize(size)+"["+filetype+"]", true)
 	
-	doLogging("Saving to:"+fileName, true)
+	doLogging("Saving to: "+fileName, true)
 	doLogging("", true)
-	return resp
+	return resp, a
 }
 
 func GetPort(s string) (port string) {
@@ -113,7 +115,13 @@ func CalcSize(numb int64) string {
 	return output
 }
 
-func FileInfo(resp *http.Response,FileName, url string) (size int64, FileType string) {
+func FileInfo(FileName, url string) (size int64, FileType string) {
+	req, err := http.NewRequest("GET", url, nil)
+	errorChecker(err)
+	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
+	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
+	resp, err3 := http.DefaultClient.Do(req)
+	errorChecker(err3)
 
 	size = resp.ContentLength
 
--- wget/wget.go
+++ wget/wget.go
@@ -34,20 +34,21 @@ func Run() {
 		if Flags.O_Flag != "" {
 			tempFile = Flags.O_Flag
 		}
-		response := getResponse(url, httpmethod, shorturl, tempFile)
-		size, _ := FileInfo(response, tempFile, url)
-		elapsed := writeToFile(givenpath, tempFile, response)
+		response, FileSizeString := getResponse(url, httpmethod, shorturl, tempFile)
+		//size, _ := FileInfo(tempFile, url)
+		elapsed, DownloadedData := writeToFile(givenpath, tempFile, response)
 		h, _ := time.ParseDuration(elapsed.String())
+		size, _ := strconv.Atoi(FileSizeString)
 		var AvgDown float64
 		if h.Seconds() < 1 {
 			AvgDown = float64(size) * (h.Seconds()) / 10
 		} else {
 			AvgDown = float64(size) / (h.Seconds()) / 1000000
 		}
-		SizeInt := strconv.FormatInt(size, 10)
-
+		DownloadedDataInt := strconv.FormatInt(DownloadedData, 10)
+		
 		doLogging(time.Now().Format("2006-01-02 15:04:05 - Download completed! "), false)
-		doLogging("["+string(SizeInt)+"/"+string(SizeInt)+"]", true)
+		doLogging("["+string(DownloadedDataInt)+"/"+FileSizeString+"]", true)
 		doLogging("Time elapsed: ", false)
 		doLogging(shortDur(elapsed), false)
 		doLogging(" Average download speed: ", false)
@@ -95,7 +96,7 @@ func createPath(path string) string {
 }
 
 // Write the response of the GET request to file
-func writeToFile(directory, fileName string, resp *http.Response) (elapsed time.Duration) {
+func writeToFile(directory, fileName string, resp *http.Response) (elapsed time.Duration, data int64) {
 	createPath("downloads/" + directory)
 	file, err := os.OpenFile("downloads/"+directory+"/"+fileName, os.O_CREATE|os.O_WRONLY, 0777)
 	errorChecker(err)
@@ -109,10 +110,10 @@ func writeToFile(directory, fileName string, resp *http.Response) (elapsed time.
 		resp.ContentLength,
 		"Downloading: "+fileName,
 	)
-	_, err = io.Copy(io.MultiWriter(bufferedWriter, bar), resp.Body)
+	data, err = io.Copy(io.MultiWriter(bufferedWriter, bar), resp.Body)
 	errorChecker(err)
 	t := time.Now()
 	elapsed = t.Sub(start)
 
-	return elapsed
+	return elapsed, data
 }
